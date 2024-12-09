dd6e2b8cd16c447d5d73409488b3517b47311b04
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-12-09 18:47:01 +0000

    01 - Historian Hysteria

---
+++ 01_historian_hysteria.py
@@ -0,0 +1,17 @@
+input = open('01_input.txt','r')
+left = []
+right = []
+for line in input.readlines():
+    items = line.split()
+    left.append(items[0])
+    right.append(items[1])
+left = sorted(left)
+right = sorted(right)
+print(left, right)
+distance = 0
+i = 0
+while i < len(left) :
+    distance = distance + abs(int(right[i]) - int(left[i]))
+    i = i+1
+print(distance)
+
