0226d9cb828ceb15656aedaa36ada0fd9a7f027a
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2024-12-10 17:26:10 +0000

    01 - Historian Hysteria

--- 01_historian_hysteria.py
+++ 01_historian_hysteria.py
@@ -1,4 +1,8 @@
+# https://adventofcode.com/2024/day/1
+
 input = open('01_input.txt','r')
+
+# Part 1
 left = []
 right = []
 for line in input.readlines():
@@ -13,5 +17,21 @@ while i < len(left) :
     distance = distance + abs(int(right[i]) - int(left[i]))
     i = i+1
 print(distance)
+
+# Part 2
+repetitions = {}
+for i in right:
+    if i in repetitions:
+        repetitions[i] = repetitions[i] + 1
+    else:
+        repetitions[i] = 1
+similarity = 0
+for i in left:
+    if i in repetitions:
+        similarity = similarity + (int(i)*repetitions[i])
+print(similarity)
+
+
+
 
 
